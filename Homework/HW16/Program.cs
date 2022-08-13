a0b268b2ebeb6f821ce072af6d0d6bf79fdb5330
AndreyanovD <andreyanov_de@mail.ru>
2022-08-13 13:08:32 +0300

    Добавлены ДЗ по 6 семинару

---
+++ Homework/HW16/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+void Zadacha41()
+{
+    Console.Write("Введите количество чисел, которые Вы хотите ввести: ");
+    int count = Convert.ToInt32(Console.ReadLine());
+    PositiveCount(count);
+}
+
+void PositiveCount(int num)
+{
+    int count = 0;
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number > 0) count++;
+    }
+    Console.WriteLine($"Вы ввели {count} положительных чисел.");
+}
+
+Zadacha41();
+
---
+++ Homework/HW17/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+void Zadacha43()
+{
+    double[] coords1 = GetFunction(1);
+    double[] coords2 = GetFunction(2);
+    double x = (coords2[1] - coords1[1]) / (coords1[0] - coords2[0]);
+    double y = coords1[0] * x + coords1[1];
+    Console.WriteLine($"Координаты пересечения прямых X = {x}, Y = {y}.");
+}
+
+double[] GetFunction(int n)
+{
+    double[] array = new double[2];
+    Console.Write($"Введите коэффициент k{n} уравнения y=k{n}*x + b{n}: ");
+    array[0] = Convert.ToInt32(Console.ReadLine());
+    Console.Write($"Введите коэффициент b{n} уравнения y=k{n}*x + b{n}: ");
+    array[1] = Convert.ToInt32(Console.ReadLine());
+    return array;
+}
+
+Zadacha43();
